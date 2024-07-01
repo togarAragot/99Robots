8b29f3e4ff05bc148cf3c7f41327ede52b62d930
togarAragot <the.aragot@gmail.com>
2024-07-01 20:58:26 +0200

    Single quotes maybe bad?

--- src/services/server_hopper.lua
+++ src/services/server_hopper.lua
@@ -11,7 +11,7 @@ local function GetServers(placeid)
 
 	print("pre concat")
 	print(tostring(placeId))
-	local ListRaw = game:HttpGet('https://games.roblox.com/v1/games/' .. tostring(placeId) .. '/servers/0?sortOrder=2&excludeFullGames=true&limit=100')
+	local ListRaw = game:HttpGet("https://games.roblox.com/v1/games/" .. tostring(placeId) .. "/servers/0?sortOrder=2&excludeFullGames=true&limit=100")
 	print("post concat")
 	local CurrentList = httpService:JSONDecode(ListRaw)
 	print("post json")
