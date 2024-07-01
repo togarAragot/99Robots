45ce3f775ed0afbf9a7ba97a1ecf2898c2d79859
togarAragot <the.aragot@gmail.com>
2024-07-01 20:23:11 +0200

    Added lil log message

--- src/main.lua
+++ src/main.lua
@@ -1,8 +1,10 @@
-local module = loadstring(game:HttpGet(""))() -- Load ServerHopper
-local messages = loadstring(game:HttpGet(""))() -- Load messages
+local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/togarAragot/99Robots/main/src/services/server_hopper.lua?token=GHSAT0AAAAAACTWI5MYN2Z3RDEV32J5SHOOZUC3UKQ"))() -- Load ServerHopper
+local messages = loadstring(game:HttpGet("https://raw.githubusercontent.com/togarAragot/99Robots/main/src/template-messages.lua?token=GHSAT0AAAAAACTWI5MZMAFZJ24GR2ZXC44UZUC6GPA"))() -- Load messages
 while true do
     local message = messages[math.random(#messages)]
-    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message)
+    local TextChatService = game:GetService("TextChatService")
+    local generalChannel: TextChannel = TextChatService:WaitForChild("TextChannels").RBXGeneral
+    generalChannel:SendAsync(message)
     module:Teleport(game.placeId)
     wait()
 end
