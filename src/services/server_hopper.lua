bc46640dc192d41c6ecb9385010c457d893e93e5
togarAragot <the.aragot@gmail.com>
2024-10-22 23:11:40 +0200

    test

--- src/services/server_hopper.lua
+++ src/services/server_hopper.lua
@@ -26,42 +26,6 @@ local function GetNextServer()
 	local success, response = pcall(function()
 		return httpService:JSONDecode(responseRaw)
 	end)
-	
-	-- Check if decoding succeeded
-	if success then
-		print('Decoded response:'.. tostring(response))
-		print('this is the id with string' .. tostring(response.id))
-		print('this is the id without string' .. response.id)
-	else
-		print('Failed to decode JSON: ' .. tostring(responseRaw))
-	end
-    
-    -- Uncomment the following block if you want to use PostAsync to get server data.
-    -- Ensure that the URL is correct (added http:// as mentioned in earlier discussions).
-    
-    -- local response = httpService:PostAsync('http://127.0.0.1:8000/api/99robots/next', data, Enum.HttpContentType.ApplicationJson, false)
-    -- print('raw list' .. tostring(response))
-    -- local nextServerData = httpService:JSONDecode(response)
-
-    -- print('serverData' .. tostring(nextServerData))
-    -- if nextServerData == nil then
-    --     game.StarterGui:SetCore('SendNotification', {
-    --         Title = 'ERROR';
-    --         Text = 'Something went wrong with grabbing servers (data doesn\'t exist)';
-    --         Icon = 'rbxassetid://2541869220';
-    --         Duration = 7;
-    --     })
-    --     return {}
-    -- end
-
-    -- Additional check for server having room
-    -- for i = 1, #CurrentList.data do
-    --     if CurrentList ~= nil then
-    --         if CurrentList.data[i].maxPlayers - 1 > CurrentList.data[i].playing then
-    --             table.insert(Servers, CurrentList.data[i])
-    --         end
-    --     end
-    -- end
 
     wait()
 
@@ -76,7 +40,7 @@ function module:Teleport(placeId)
     print(serverId)
 	print('post servers')
     -- Uncomment this when nextServer data is available from GetServers
-    -- teleportService:TeleportToPlaceInstance(placeId, nextServer.id, game.Players.LocalPlayer)
+    teleportService:TeleportToPlaceInstance(placeId, serverId, game.Players.LocalPlayer)
 end
 
 return module
