f2b935329c580c7ba3ae2695a4236e34e4516e66
togarAragot <the.aragot@gmail.com>
2024-10-22 22:18:59 +0200

    update!

--- src/services/server_hopper.lua
+++ src/services/server_hopper.lua
@@ -1,39 +1,43 @@
-local lastTimeStamp = os.time(os.date("!*t"))
-local teleportService = game:GetService("TeleportService")
-local httpService = game:GetService("HttpService")
+local lastTimeStamp = os.time(os.date('!*t'))
+local teleportService = game:GetService('TeleportService')
+local httpService = game:GetService('HttpService')
 
 local function GetServers(placeId)
     local Servers = {}
-    
+
     if placeId == nil then
-        placeId = game.PlaceId
+        placeId = game.PlaceId  -- Corrected capitalization
     end
 
-    print("pre servers")
+    print('pre servers')
     local data = '{ "occupier": '.. game.Players.LocalPlayer.DisplayName ..'}'
-    --local response = httpService::PostAsync("127.0.0.1:8000/api/99robots/next", data, Enum.HttpContentType.ApplicationJson, false)
-    --print("raw list" .. tostring(response))
-    --local nextServerData = httpService:JSONDecode(response)
+    
+    -- Uncomment the following block if you want to use PostAsync to get server data.
+    -- Ensure that the URL is correct (added http:// as mentioned in earlier discussions).
+    
+    -- local response = httpService:PostAsync('http://127.0.0.1:8000/api/99robots/next', data, Enum.HttpContentType.ApplicationJson, false)
+    -- print('raw list' .. tostring(response))
+    -- local nextServerData = httpService:JSONDecode(response)
 
-    --print("serverData" .. tostring(nextServerData))
-    --if nextServerData == nil then
-    --  game.StarterGui:SetCore("SendNotification", {
-    --      Title = "ERROR";
-    --      Text = "Something went wrong with grabbing servers (data doesn't exist)";
-    --      Icon = "rbxassetid://2541869220";
-    --      Duration = 7;
-    --  })
-    --  return {}
-    --end
+    -- print('serverData' .. tostring(nextServerData))
+    -- if nextServerData == nil then
+    --     game.StarterGui:SetCore('SendNotification', {
+    --         Title = 'ERROR';
+    --         Text = 'Something went wrong with grabbing servers (data doesn\'t exist)';
+    --         Icon = 'rbxassetid://2541869220';
+    --         Duration = 7;
+    --     })
+    --     return {}
+    -- end
 
     -- Additional check for server having room
-    --for i = 1,#CurrentList.data do
-    --  if CurrentList ~= nil then
-    --      if CurrentList.data[i].maxPlayers - 1 > CurrentList.data[i].playing then
-    --          table.insert(Servers, CurrentList.data[i])
-    --      end
-    --  end
-    --end
+    -- for i = 1, #CurrentList.data do
+    --     if CurrentList ~= nil then
+    --         if CurrentList.data[i].maxPlayers - 1 > CurrentList.data[i].playing then
+    --             table.insert(Servers, CurrentList.data[i])
+    --         end
+    --     end
+    -- end
 
     wait()
 
@@ -42,13 +46,12 @@ end
 
 local module = {}
 function module:Teleport(placeId)
-    print("method called")
+    print('method called')
 
+    GetServers(game.PlaceId)  -- Corrected capitalization
 
-    GetServers(game.placeId)
-
-    --teleportService:TeleportToPlaceInstance(placeId, nextServer.id, game.Players.LocalPlayer)
-
-
+    -- Uncomment this when nextServer data is available from GetServers
+    -- teleportService:TeleportToPlaceInstance(placeId, nextServer.id, game.Players.LocalPlayer)
 end
+
 return module
