966a5853c68de33f9fde910127618dfc8575a46f
togarAragot <the.aragot@gmail.com>
2024-10-22 22:12:46 +0200

    test

--- src/main.lua
+++ src/main.lua
@@ -1,10 +1,12 @@
-local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/togarAragot/99Robots/main/src/services/server_hopper.lua?token=GHSAT0AAAAAACTWI5MYN2Z3RDEV32J5SHOOZUC3UKQ"))() -- Load ServerHopper
-local messages = loadstring(game:HttpGet("https://raw.githubusercontent.com/togarAragot/99Robots/main/src/template-messages.lua?token=GHSAT0AAAAAACTWI5MZMAFZJ24GR2ZXC44UZUC6GPA"))() -- Load messages
+local serverHopperScript = game:HttpGet("https://raw.githubusercontent.com/togarAragot/99Robots/refs/heads/main/src/services/server_hopper.lua")
+print(serverHopperScript)
+local module = loadstring(serverHopperScript)() -- Load ServerHopper
+local messages = loadstring(game:HttpGet("https://raw.githubusercontent.com/togarAragot/99Robots/refs/heads/main/src/template-messages.lua"))() -- Load messages
 while true do
     local message = messages[math.random(#messages)]
     local TextChatService = game:GetService("TextChatService")
     local generalChannel: TextChannel = TextChatService:WaitForChild("TextChannels").RBXGeneral
     generalChannel:SendAsync(message)
     module:Teleport(game.PlaceId)
-    wait()
+    wait(5)
 end
--- src/services/server_hopper.lua
+++ src/services/server_hopper.lua
@@ -1,54 +1,53 @@
 local lastTimeStamp = os.time(os.date("!*t"))
 local teleportService = game:GetService("TeleportService")
 local httpService = game:GetService("HttpService")
-local lastServers = {}
 
 local function GetServers(placeId)
     local Servers = {}
-	
-	if placeId == nil then
-		placeId = game.PlaceId
-	end
+    
+    if placeId == nil then
+        placeId = game.PlaceId
+    end
 
-	print("pre servers")
-	local data = '{ "occupier": '.. game.Players.LocalPlayer.DisplayName ..'}'
-	local response = httpService::PostAsync("127.0.0.1:8000/api/99robots/next", data, Enum.HttpContentType.ApplicationJson, false)
-	print("raw list" .. tostring(response))
-	local nextServerData = httpService:JSONDecode(response)
+    print("pre servers")
+    local data = '{ "occupier": '.. game.Players.LocalPlayer.DisplayName ..'}'
+    --local response = httpService::PostAsync("127.0.0.1:8000/api/99robots/next", data, Enum.HttpContentType.ApplicationJson, false)
+    --print("raw list" .. tostring(response))
+    --local nextServerData = httpService:JSONDecode(response)
 
-	print("serverData" .. tostring(nextServerData))
-	if nextServerData == nil then
-		game.StarterGui:SetCore("SendNotification", {
-			Title = "ERROR";
-			Text = "Something went wrong with grabbing servers (data doesn't exist)";
-			Icon = "rbxassetid://2541869220";
-			Duration = 7;
-		})
-		return {}
-	end
+    --print("serverData" .. tostring(nextServerData))
+    --if nextServerData == nil then
+    --  game.StarterGui:SetCore("SendNotification", {
+    --      Title = "ERROR";
+    --      Text = "Something went wrong with grabbing servers (data doesn't exist)";
+    --      Icon = "rbxassetid://2541869220";
+    --      Duration = 7;
+    --  })
+    --  return {}
+    --end
 
-	-- Additional check for server having room
-	--for i = 1,#CurrentList.data do
-	--	if CurrentList ~= nil then
-	--		if CurrentList.data[i].maxPlayers - 1 > CurrentList.data[i].playing then
-	--			table.insert(Servers, CurrentList.data[i])
-	--		end
-	--	end
-	--end
+    -- Additional check for server having room
+    --for i = 1,#CurrentList.data do
+    --  if CurrentList ~= nil then
+    --      if CurrentList.data[i].maxPlayers - 1 > CurrentList.data[i].playing then
+    --          table.insert(Servers, CurrentList.data[i])
+    --      end
+    --  end
+    --end
 
-	wait()
+    wait()
 
     return Servers
 end
 
 local module = {}
 function module:Teleport(placeId)
-	print("method called")
+    print("method called")
 
 
-	GetServers(game.placeId)
+    GetServers(game.placeId)
 
-	--teleportService:TeleportToPlaceInstance(placeId, nextServer.id, game.Players.LocalPlayer)
+    --teleportService:TeleportToPlaceInstance(placeId, nextServer.id, game.Players.LocalPlayer)
 
 
 end
